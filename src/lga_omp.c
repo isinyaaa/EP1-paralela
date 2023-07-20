4bffe42800c238b89a6e42245b9f76c5b10d04b5
Isabella Basso <isabellabdoamaral@usp.br>
2023-07-20 12:53:08 -0300

    remove redundant `get_next_cell` declarations

--- src/lga_omp.c
+++ src/lga_omp.c
@@ -2,31 +2,6 @@
 #include "lga_base.h"
 #include "lga_omp.h"
 
-static byte get_next_cell(int i, int j, byte *grid_in, int grid_size) {
-    byte next_cell = EMPTY;
-
-    for (int dir = 0; dir < NUM_DIRECTIONS; dir++) {
-        int rev_dir = (dir + NUM_DIRECTIONS/2) % NUM_DIRECTIONS;
-        byte rev_dir_mask = 0x01 << rev_dir;
-
-        int di = directions[i%2][dir][0];
-        int dj = directions[i%2][dir][1];
-        int n_i = i + di;
-        int n_j = j + dj;
-
-        if (inbounds(n_i, n_j, grid_size)) {
-            if (grid_in[ind2d(n_i,n_j)] == WALL) {
-                next_cell |= from_wall_collision(i, j, grid_in, grid_size, dir);
-            }
-            else if (grid_in[ind2d(n_i, n_j)] & rev_dir_mask) {
-                next_cell |= rev_dir_mask;
-            }
-        }
-    }
-
-    return check_particles_collision(next_cell);
-}
-
 static void update(byte *grid_in, byte *grid_out, int grid_size,
                    int start, int end) {
     for (int i = start; i < end; i++) {
